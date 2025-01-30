ff89953a5b99a6fe4c35d6188382caaecfaaadc7
Param Arora <param2k3@gmail.com>
2025-01-31 00:41:46 +0530

    potential migration fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250114_010454_initial from './20250114_010454_initial';
 import * as migration_20250130_184210 from './20250130_184210';
+import * as migration_20250130_190930 from './20250130_190930';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250130_184210.up,
     down: migration_20250130_184210.down,
-    name: '20250130_184210'
+    name: '20250130_184210',
+  },
+  {
+    up: migration_20250130_190930.up,
+    down: migration_20250130_190930.down,
+    name: '20250130_190930'
   },
 ];
